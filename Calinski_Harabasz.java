f61957a17336212a220b5c8de74072c8d9230e97
AlexDem1126 <alexdem1126@gmail.com>
2017-10-03 12:56:10 -0500

    ch

--- Calinski_Harabasz.java
+++ Calinski_Harabasz.java
@@ -6,7 +6,10 @@ public class Calinski_Harabasz {
 	private double[][] dataset;
 	private int numOfPoints;
 	private int numOfDimension;
-
+	private int numOfClusters;
+	private double[][] centroids;
+	private double threshold;
+	
 
 	
 	
@@ -16,7 +19,44 @@ public class Calinski_Harabasz {
 		numOfDimension = numOfDimensionMF;
 	}
 	
+			
+
+	public void kMeanClusteringForCH(int nClusters, int nIterations, double iniThreshold, double[][] iniCentroids) {
+		numOfClusters = nClusters;
+		threshold = iniThreshold;
+				
+		// 1. generate pseudo random centroids
+		centroids = generateCentroids(); 			//initial centroid
 		
+		
+	}
+	
 	
 
+	// 1. generate pseudo random centroids
+	private double[][] generateCentroids() {
+		centroids = new double[numOfClusters][];
+		ArrayList centrdData = new ArrayList();
+					
+		for (int i = 0; i < numOfClusters; i++) {
+			int randomCntrd;
+			//generate unique random centroids without duplicates
+			do {
+				randomCntrd = (int) (Math.random() * numOfPoints);
+			} while (centrdData.contains(randomCntrd));
+			
+			centrdData.add(randomCntrd);
+			
+			// copy centroid's attributes to the generated centroid
+			centroids[i] = new double[numOfDimension];			
+			for (int j = 0; j < numOfDimension; j++) {
+				centroids[i][j] = dataset[randomCntrd][j];
+			}
+		}
+		return centroids;
+	}
+	
+	
+	
+	
 }
