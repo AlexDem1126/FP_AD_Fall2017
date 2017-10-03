0398d833b4a730eac4685df21a9c28a14e2052d3
AlexDem1126 <alexdem1126@gmail.com>
2017-10-03 12:53:09 -0500

    ch

---
+++ Calinski_Harabasz.java
@@ -0,0 +1,22 @@
+package FP_AD_Fall2017;
+
+import java.util.ArrayList;
+
+public class Calinski_Harabasz {
+	private double[][] dataset;
+	private int numOfPoints;
+	private int numOfDimension;
+
+
+	
+	
+	public Calinski_Harabasz(double[][] datasetMF, int numOfPointsMF, int numOfDimensionMF) {
+		dataset = datasetMF;
+		numOfPoints = numOfPointsMF;
+		numOfDimension = numOfDimensionMF;
+	}
+	
+		
+	
+
+}
