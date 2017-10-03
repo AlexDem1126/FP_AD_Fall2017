68124977ad0e6a4c2399459af84b0ff785798ce8
AlexDem1126 <alexdem1126@gmail.com>
2017-10-03 12:42:34 -0500

    main

--- myMain.java
+++ myMain.java
@@ -20,32 +20,81 @@ public class myMain {
 				
 
 				
-				kmeans objKmeans = new kmeans(objMF.getDataset(), objMF.getNumOfPoints(), objMF.getNumOfDimension());
+				//******************************************************************
+				/*********** Calinski-Harabasz Index (CH) ***********/
+				//******************************************************************
+				Calinski_Harabasz objCH = new Calinski_Harabasz(objMF.getDataset(), objMF.getNumOfPoints(), objMF.getNumOfDimension());
+								
+				System.out.println("***** Calinski-Harabasz Index (CH) *****");
+				int k_Min = 2;
+				int k_Max = (int)Math.sqrt(objMF.getNumOfPoints() / 2);
+				System.out.println("k_Max: " + k_Max);
+				
+				double[] sSW_R = new double[R]; 	//for run number R: sum of squared within-cluster scatter matrix (SSW) 
+				double[] sSB_R = new double[R]; 	//for run number R: sum of squared between-cluster scatter matrix (SSB) 				
+				
+				double[] CH = new double[k_Max - 1]; //Calinski-Harabasz Index (CH)
+				int countCH = 0;
+				
+				while(k_Min <= k_Max){
+					int count = 0;
+					System.out.println("\n===== Calinski-Harabasz Index (CH) for K = " + k_Min + " =====");
+					int R_CH = R;
+					for (int i=0; i<R; i++){
+						sSW_R[i] = 0;
+						sSB_R[i] = 0;
+					}
+					
+					while (R_CH > 0) {
+						System.out.println("\nRun " + (count + 1) + "\n============================");
+						objCH.kMeanClusteringForCH(k_Min, I, T, null); //cluster, iteration, threshold, centroid
+						sSW_R[count] = objCH.getSSW();
+						sSB_R[count] = objCH.getSSB();						
+						R_CH--;
+						count++;
+					}
+					int smallestSSW = objCH.findSmallestSSW(sSW_R); //find smallest index of SSW
+					double sSW_Smallest = sSW_R[smallestSSW];
+					double sSB_Highest = sSB_R[smallestSSW];
+					CH[countCH] = objCH.kCalinski_Harabasz_Index(sSW_Smallest, sSB_Highest);
+					System.out.println("For K = " + k_Min + ", CH = " + CH[countCH]);
+					System.out.println("\n");
+					k_Min++; countCH++;
+				}
+				//find the largest CH index, which will represent the best number of clusters
+				int k_CH = objCH.findLargestCH(CH);
+				System.out.println("The best number of clusters = " + k_CH);
+//				System.out.println("The largest CH value = " + largestCH);
+				
+				
+				/*********** END Calinski-Harabasz Index (CH) ***********/	
 				
 				
 				
 				//******************************************************************
 				/*********** Kmeans ***********/
 				//******************************************************************
-				int count = 0;
-				System.out.println("***** Kmeans *****");
+				kmeans objKmeans = new kmeans(objMF.getDataset(), objMF.getNumOfPoints(), objMF.getNumOfDimension());
+				
+				int count_K = 0;
+				System.out.println("\n***** Kmeans *****");
 				while (R > 0) {
-					System.out.println("\nRun " + (count + 1) + "\n============================");
-					objKmeans.kMeanClustering(K, I, T, null); //cluster, iteration, threshold, centroid
+					System.out.println("\nRun " + (count_K + 1) + "\n============================");
+					objKmeans.kMeanClustering(k_CH, I, T, null); //cluster, iteration, threshold, centroid
 					R--;
-					count++;
+					count_K++;
 				}
-
+				/*********** END Kmeans ***********/
 				
 				
 			} catch (NumberFormatException e) {
 				System.out.println(
-						"Input format should be: <file name.txt> or <file name.csv>");
+						"Input format should be: <file name.data> or <file name.txt> or <file name.csv>");
 				System.exit(1);
 			}
 		} else {
 			System.out.println(
-					"Input format should be: <file name.txt> or <file name.csv>");
+					"Input format should be: <file name.data> or <file name.txt> or <file name.csv>");
 		}	
 
 	}
